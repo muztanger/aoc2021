029d7acd79719c3d6afa02de8e2a5071bf1476a8
Johan Östrand <muztanger@gmail.com>
2021-12-02 11:28:06 +0000

    Day2 finished. Error handling in parser needs work...

--- src/day02.rs
+++ src/day02.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::str::FromStr;
-use std::num::ParseIntError;
 use std::fmt;
 
 struct Command {
@@ -51,6 +50,25 @@ pub fn day2part1() -> i64 {
     horizontal * depth
 }
 
+pub fn day2part2() -> i64 {
+    let commands = read_data();
+    
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for command in commands {
+        match command.command.as_str() {
+            "forward" => {horizontal += command.value; depth += aim * command.value},
+            "down" => {aim += command.value},
+            "up" => {aim -= command.value},
+            _ => {}
+        };
+    }
+
+    println!("{} {}", horizontal, depth);
+    horizontal * depth
+}
 
 fn read_data() -> Vec<Command> {
     let values: String = fs::read_to_string("data/day02.txt").expect("Could not read file");
@@ -67,14 +85,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
-        assert_eq!(150, day2part1());
+    fn test_day2part1() {
+        assert_eq!(1660158, day2part1());
     }
 
-    // #[test]
-    // fn test_bad_add() {
-    //     // This assert would fire and test will fail.
-    //     // Please note, that private functions can be tested too!
-    //     assert_eq!(bad_add(1, 2), 3);
-    // }
+    #[test]
+    fn test_day2part2() {
+        assert_eq!(1604592846, day2part2());
+    }
 }
