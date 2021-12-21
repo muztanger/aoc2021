2495f7fb3e6b68837a0d2bee3854a23b9b74f356
Johan Östrand <muztanger@gmail.com>
2021-12-21 22:48:46 +0000

    day8 part2 work in progress About to switch algorithm

--- src/day08.rs
+++ src/day08.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::{HashMap,HashSet};
 
 pub fn part1() -> i128 {
     let lines = read_data();
@@ -15,7 +16,62 @@ pub fn part1() -> i128 {
 
 
 pub fn part2() -> i128 {
-    1
+    let lines = _read_example();
+    let mut count = 0;
+    for line in lines.iter().filter(|x| x.len() > 0) {
+        println!("Line: {}", &line);
+        let line: Vec<&str> = line.split("|").collect::<Vec<&str>>();
+        assert_eq!(2, line.len());
+        let input: Vec<&str> = line[0].split_whitespace().collect();
+
+        let mut candidates: HashMap<char, HashSet<char>> = HashMap::new();
+        let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+        for c in letters {
+            candidates.insert(c, letters.iter().cloned().collect());
+        }
+
+        //  aaaa
+        // b    c
+        // b    c
+        //  dddd
+        // e    f
+        // e    f
+        //  gggg
+        for word in input {
+            let remove: Vec<char> = match word.len() {
+                // Digit 1
+                2 => {vec!['a', 'b', 'd', 'e', 'g']},
+                
+                // Digit 4
+                4 => {vec!['a', 'b', 'g']},
+
+                // Digit 7
+                3 => {vec!['b', 'd', 'e', 'g']},
+
+                _ => {vec![]}
+            };
+            for letter in &remove {
+                let cand = candidates.get_mut(letter).unwrap();
+                for w in word.chars() {
+                    cand.remove(&w);
+                }
+            }
+        }
+        let mut keys = candidates.keys().copied().collect::<Vec<_>>();
+        keys.sort();
+        for candidate in keys {
+            let mut values = candidates.get(&candidate).unwrap().iter().map(|x| x.to_string()).collect::<Vec<String>>();
+            values.sort();
+            println!("{}: {}", candidate, values.join(","));
+        }
+
+        let output: Vec<&str> = line[1].split_whitespace().collect();
+
+        //1, 4, 7, and 8
+        //2, 4, 3, 7
+        count += output.iter().filter(|x| x.len() == 2 || x.len() == 4 || x.len() == 3 || x.len() == 7).count();
+    }
+    count as i128
 }
 
 fn read_data() -> Vec<String> {
@@ -41,6 +97,6 @@ mod tests {
 
     #[test]
     fn test_day8part2() {
-        assert_eq!(86397080, part2());
+        assert_eq!(61229, part2());
     }
 }
