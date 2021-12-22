9da73cae8552314dccf4188328ef5676d87c2d01
Johan Östrand <muztanger@gmail.com>
2021-12-22 22:25:50 +0000

    day8 part2 example works

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs;
 use std::collections::{HashMap,HashSet};
 
@@ -19,57 +20,172 @@ pub fn part2() -> i128 {
     let lines = _read_example();
     let mut count = 0;
     for line in lines.iter().filter(|x| x.len() > 0) {
-        println!("Line: {}", &line);
+        //println!("Line: {}", &line);
         let line: Vec<&str> = line.split("|").collect::<Vec<&str>>();
         assert_eq!(2, line.len());
         let input: Vec<&str> = line[0].split_whitespace().collect();
 
-        let mut candidates: HashMap<char, HashSet<char>> = HashMap::new();
-        let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-        for c in letters {
-            candidates.insert(c, letters.iter().cloned().collect());
+        let mut candidates: Vec<Vec<char>> = Vec::new();
+
+        let letters = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+        let mut lights: HashMap<char, Vec<usize>> = HashMap::new();
+        lights.insert('0', vec![0, 1, 2, 4, 5, 6]);
+        lights.insert('1', vec![2, 5]);
+        lights.insert('2', vec![0, 2, 3, 4, 6]);
+        lights.insert('3', vec![0, 2, 3, 5, 6]);
+        lights.insert('4', vec![1, 2, 3, 5]);
+        lights.insert('5', vec![0, 1, 3, 5, 6]);
+        lights.insert('6', vec![0, 1, 3, 4, 5, 6]);
+        lights.insert('7', vec![0, 2, 5]);
+        lights.insert('8', vec![0, 1, 2, 3, 4, 5, 6]);
+        lights.insert('9', vec![0, 1, 2, 3, 5]);
+
+        for perm in letters.iter().permutations(letters.len()).unique() {
+            let mut is_valid = true;
+            'word: for word in &input {
+                match word.len() {
+                    // Digit 1
+                    2 => {
+                        for x in lights.get(&'1').unwrap() {
+                            if word.chars().all(|c| *perm[*x] != c) {
+                                is_valid = false;
+                                break 'word;
+                            }
+                        }
+                    },
+                    // Digit 7
+                    3 => {
+                        for x in lights.get(&'7').unwrap() {
+                            if word.chars().all(|c| *perm[*x] != c) {
+                                is_valid = false;
+                                break 'word;
+                            }
+                        }
+                    },
+
+                    // Digit 4
+                    4 => {
+                        for x in lights.get(&'4').unwrap() {
+                            if word.chars().all(|c| *perm[*x] != c) {
+                                is_valid = false;
+                                break 'word;
+                            }
+                        }
+                    },
+
+                    // Digit 2, 3 or 5
+                    5 => {
+                        let mut found_valid = true;
+
+                        // 2
+                        for x in lights.get(&'2').unwrap() {
+                            if word.chars().all(|c| *perm[*x] != c) {
+                                found_valid = false;
+                                break;
+                            }
+                        }
+
+                        if !found_valid {
+                            // 3
+                            found_valid = true;
+                            for x in lights.get(&'3').unwrap() {
+                                if word.chars().all(|c| *perm[*x] != c) {
+                                    found_valid = false;
+                                    break;
+                                }
+                            }
+                        }
+                    
+                        if !found_valid {
+                            // 5
+                            found_valid = true;
+                            for x in lights.get(&'5').unwrap() {
+                                if word.chars().all(|c| *perm[*x] != c) {
+                                    found_valid = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if !found_valid {
+                            is_valid = false;
+                            break 'word;
+                        }
+                    }
+    
+    
+                    _ => {}
+                };
+            }
+
+            if is_valid {
+                candidates.push(perm.iter().copied().map(|x| *x).collect())
+            }
         }
 
-        //  aaaa
-        // b    c
-        // b    c
-        //  dddd
-        // e    f
-        // e    f
-        //  gggg
-        for word in input {
-            let remove: Vec<char> = match word.len() {
-                // Digit 1
-                2 => {vec!['a', 'b', 'd', 'e', 'g']},
-                
-                // Digit 4
-                4 => {vec!['a', 'b', 'g']},
 
-                // Digit 7
-                3 => {vec!['b', 'd', 'e', 'g']},
+        // for candidate in candidates {
+        //     println!("{} -> {}",
+        //     letters.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(", "),
+        //     candidate.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(", "));
+        // }
+        assert_eq!(1, candidates.len());
+        let code = candidates.first().unwrap();
 
-                _ => {vec![]}
-            };
-            for letter in &remove {
-                let cand = candidates.get_mut(letter).unwrap();
-                for w in word.chars() {
-                    cand.remove(&w);
+        let output: Vec<&str> = line[1].split_whitespace().collect();
+        let mut x: Vec<char> = Vec::new();
+        for number in output {
+                //  0000    aaaa
+                // 1    2  b    c 
+                // 1    2  b    c
+                //  3333    dddd
+                // 4    5  e    f
+                // 4    5  e    f
+                //  6666    gggg
+                match number.len() {
+                2 => {x.push('1');},
+                3 => {x.push('7');},
+                4 => {x.push('4');},
+                5 => {
+                    //2, 3 or 5
+                    if number.chars().contains(&code[1]) {
+                        // b->5
+                        x.push('2');
+                    } else if number.chars().contains(&code[4]) {
+                        // e->2
+                        x.push('3');
+                    }
+                    else {
+                        x.push('5');
+                    }
+                    // else 3
+
+                },
+                6 => {
+                    //0, 6 or 9
+                    if number.chars().contains(&code[3]) {
+                        if number.chars().contains(&code[4]) {
+                            x.push('6');
+                        } else {
+                            x.push('9');
+                        }
+                    } else {
+                        x.push('0');
+                    }
+                },
+                7 => {x.push('8');}
+                _ => {
+                    //fail!
                 }
             }
         }
-        let mut keys = candidates.keys().copied().collect::<Vec<_>>();
-        keys.sort();
-        for candidate in keys {
-            let mut values = candidates.get(&candidate).unwrap().iter().map(|x| x.to_string()).collect::<Vec<String>>();
-            values.sort();
-            println!("{}: {}", candidate, values.join(","));
-        }
+        let x: i128 = x.iter().map(|c| c.to_string()).collect::<Vec<String>>().concat().parse::<i128>().unwrap();
 
-        let output: Vec<&str> = line[1].split_whitespace().collect();
 
         //1, 4, 7, and 8
         //2, 4, 3, 7
-        count += output.iter().filter(|x| x.len() == 2 || x.len() == 4 || x.len() == 3 || x.len() == 7).count();
+        count += x;
     }
     count as i128
 }
